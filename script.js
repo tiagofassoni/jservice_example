f11eac82fc19bf75bcfe4ef70746f6e8f3768ed1
Tiago Fassoni <tiagofassoni@protonmail.com>
2023-11-08 12:36:47 -0300

    Initial version

---
+++ script.js
@@ -0,0 +1,21 @@
+my_global = undefined;
+my_clues = undefined;
+console.clear();
+$('#btn-jservice').on('click', function() {
+    $.getJSON('https://jservice.io/api/categories', 
+    {count: 10},
+    function(data){
+        my_global = data;
+        console.table(my_global);
+    })
+})
+
+$("#btn-get-clues").on('click', function() {
+    $.getJSON('https://jservice.io/api/clues',
+    {category: 61},
+    function(data) {
+        my_clues = data;
+        console.log(my_clues);
+    }
+    )
+})
